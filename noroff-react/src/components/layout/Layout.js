eea9306698b59a21952e8290a91411a91d27a9e8
Martha264 <marthahrosenlund@gmail.com>
2020-06-05 20:23:18 +0100

    meny og fav icons fixet

--- noroff-react/src/components/layout/Layout.js
+++ noroff-react/src/components/layout/Layout.js
@@ -8,6 +8,7 @@ import {
     NavLink,
 } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 import Home from "../home/Home.js";
 import Bio from "../bio/Bio.js";
@@ -35,19 +36,19 @@ function Layout() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <NavLink to="/" exact className="nav-link"> Home </NavLink>
+                        <NavLink to="/" exact className="nav-link"> HOME </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/bio" className="nav-link"> Bio </NavLink>
+                        <NavLink to="/bio" className="nav-link"> BIO </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/gallery" className="nav-link"> Gallery </NavLink>
+                        <NavLink to="/gallery" className="nav-link"> GALLERY </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/disco" className="nav-link"> Disco </NavLink>
+                        <NavLink to="/disco" className="nav-link"> DISCOGRAPHY </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/merch" className="nav-link"> Merch </NavLink>
+                        <NavLink to="/merch" className="nav-link"> MERCH </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/gigs" className="nav-link"> Gigs </NavLink>
+                        <NavLink to="/gigs" className="nav-link"> GIGS </NavLink>
                         <h2>|</h2>
-                        <NavLink to="/contact" className="nav-link"> Contact </NavLink>
+                        <NavLink to="/contact" className="nav-link"> CONTACT </NavLink>
                     </Nav>
                 </Navbar.Collapse>
                 <Navbar.Collapse className="justify-content-end">
@@ -69,7 +70,22 @@ function Layout() {
             </Container>
             <> 
                <footer className='footer mt-auto py-1 pb-4 '>
-                    <div className='container'>NOROFF 2020 &copy; Martha H. Rosenlund</div>
+                    <div className='container'>                        
+                        <a href="https://www.facebook.com/martha264/">
+                                <Image 
+                                src={require('./images/002-facebook.svg')} 
+                                alt="Facebook" 
+                                fluid 
+                                className="d-inline-block"/>
+                            </a>
+                            <a href="https://www.instagram.com/hachiko264/">
+                                <Image 
+                                src={require('./images/003-instagram.svg')} 
+                                alt="Instagram" 
+                                fluid 
+                                className="d-inline-block"/>
+                        </a>
+                    </div>
                </footer>
             </>
         </Router>
